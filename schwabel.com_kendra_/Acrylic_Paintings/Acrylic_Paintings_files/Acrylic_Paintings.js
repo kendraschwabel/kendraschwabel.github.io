595070d64c2f0e578ec5d1b04adbefe894ab18a2
Dave Schwabel <9949084+schwabel@users.noreply.github.com>
2019-02-23 19:52:35 -0600

    Initial commit.

---
+++ schwabel.com_kendra_/Acrylic_Paintings/Acrylic_Paintings_files/Acrylic_Paintings.js
@@ -0,0 +1,29 @@
+// Created by iWeb 3.0.3 local-build-20130807
+
+function createMediaStream_id3()
+{return IWCreateMediaCollection("http://www.schwabel.com/kendra/schwabel.com_kendra_/Acrylic_Paintings/Acrylic_Paintings_files/rss.xml",true,2,["No photos yet","%d photo","%d photos"],["","%d clip","%d clips"]);}
+function initializeMediaStream_id3()
+{createMediaStream_id3().load('http://www.schwabel.com/kendra/schwabel.com_kendra_/Acrylic_Paintings',function(imageStream)
+{var entryCount=imageStream.length;var headerView=widgets['widget7'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id3',{pageIndex:0}));});}
+function layoutMediaGrid_id3(range)
+{createMediaStream_id3().load('http://www.schwabel.com/kendra/schwabel.com_kendra_/Acrylic_Paintings',function(imageStream)
+{if(range==null)
+{range=new IWRange(0,imageStream.length);}
+IWLayoutPhotoGrid('id3',new IWPhotoGridLayout(2,new IWSize(284,213),new IWSize(284,32),new IWSize(336,260),27,27,0,new IWSize(2,2)),new IWEmptyStroke(),imageStream,range,(null),null,1.000000,null,'../Media/slideshow.html','widget7',null,'widget8',{showTitle:true,showMetric:false})});}
+function relayoutMediaGrid_id3(notification)
+{var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id3(range);}
+function onStubPage()
+{var args=window.location.href.toQueryParams();parent.IWMediaStreamPhotoPageSetMediaStream(createMediaStream_id3(),args.id);}
+if(window.stubPage)
+{onStubPage();}
+setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{IWRegisterNamedImage('comment overlay','../Media/Photo-Overlay-Comment.png')
+IWRegisterNamedImage('movie overlay','../Media/Photo-Overlay-Movie.png')
+loadMozillaCSS('Acrylic_Paintings_files/Acrylic_PaintingsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');NotificationCenter.addObserver(null,relayoutMediaGrid_id3,'RangeChanged','id3')
+adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');fixupIECSS3Opacity('id6');initializeMediaStream_id3()
+performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ schwabel.com_kendra_/Home_files/Home.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.3 local-build-20130807
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWEmptyStroke(),shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),stroke_2:new IWEmptyStroke(),shadow_2:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Home_files/HomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id6');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
